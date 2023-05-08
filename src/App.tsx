37286f262b5f4b113ef766a273ee222e30e659cf
Nastya <zhitnikovich3308@gmail.com>
2023-05-08 15:31:38 +0300

    init(diploma): add files to folders pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+
