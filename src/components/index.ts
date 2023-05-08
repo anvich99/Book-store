321ec998360c8b7a04bc00edd138c9300b742818
Nastya <zhitnikovich3308@gmail.com>
2023-05-08 21:12:23 +0300

    feat(components): add files to folders for components

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import { BooksList } from "./BooksList/BooksList";
+import { BookItem } from "./BookItem/BookItem";
+import { Button } from "./Button/Button";
+import { Form } from "./Form/Form";
+import { Footer } from "./Footer/Footer";
+import { Header } from "./Header/Header";
+import { Input } from "./Input/Input";
+
+export { BookItem, BooksList, Button, Form, Footer, Header, Input };
