6d88d76b10b164eeb038450355c401dd84f1d72d
Nastya <zhitnikovich3308@gmail.com>
2023-05-18 17:54:25 +0300

    feat(diploma): add firebase file

---
+++ src/store/features/bookDetails/bookDetails.ts
@@ -0,0 +1,17 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios, { AxiosError } from "axios";
+import { Book } from "types";
+// interface BookDetailsState extends Book{
+
+// }
+// const initialState:BookDetailsState= {
+
+// }
+// const BookDetailsSlice = createSlice({
+//   name: "bookDetails",
+//   initialState,
+//   reducers: {},
+//   extraReducers(builder) {
+
+//   },
+// })
